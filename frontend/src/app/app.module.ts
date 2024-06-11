d9a6ef4f8f0a5b2e184aca5168730c8b66b321a7
BachininaAnna <125889059+BachininaAnna@users.noreply.github.com>
2024-06-11 22:56:51 +0300

    main-page

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import {SharedModule} from "./shared/shared.module";
+import { MainComponent } from './views/main/main.component';
+import {CarouselModule} from "ngx-owl-carousel-o";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    CarouselModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/views/main/main.component.ts
@@ -0,0 +1,77 @@
+import {Component, OnInit} from '@angular/core';
+import {OwlOptions} from "ngx-owl-carousel-o";
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: false,
+    center: true,
+    dots: true,
+    autoHeight: true,
+    autoWidth: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      }
+    }
+  }
+
+  reviewsOptions: OwlOptions = {
+    loop: true,
+    autoplay: false,
+    center: true,
+    autoHeight: true,
+    autoWidth: true,
+    items: 3,
+    margin: 25,
+  }
+
+  services = [
+    {
+      image:'service1.png',
+      title:'Создание сайтов',
+      text:'В краткие сроки мы создадим качественный ' +
+        'самое главное продающий сайт для' +
+        'продвижения Вашего бизнеса!',
+      price: '7 500'
+    },
+    {
+      image:'service2.png',
+      title:'Продвижение',
+      text:'Вам нужен качественный ' +
+        'SMM-специалист или грамотный таргетолог?' +
+        ' Мы готовы оказать Вам услугу “Продвижения” на наивысшем уровне!',
+      price: '3 500'
+    },
+    {
+      image:'service3.png',
+      title:'Реклама',
+      text:'Без рекламы не может обойтись ни один бизнес или специалист. Обращаясь к нам, мы гарантируем быстрый прирост клиентов за счёт правильно настроенной рекламы.',
+      price: '1 000'
+    },
+    {
+      image:'service4.png',
+      title:'Копирайтинг',
+      text:'Наши копирайтеры готовы написать Вам любые продающие текста, которые не только обеспечат рост охватов, но и помогут выйти на новый уровень в продажах.',
+      price: '750'
+    },
+    ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
